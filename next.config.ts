cde5608e2a56f2d9759c943c4948597227e45b64
insideoptions <michael@insideoptions.io>
2025-12-03 23:54:08 -0800

    Force cache invalidation with unique build ID

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
 };
 
 export default nextConfig;
